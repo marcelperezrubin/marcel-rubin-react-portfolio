9e10078e9ca21f8dc754d1d865e34d9d565c2ec7
Marcel Rubin <marcelgabber89@gmail.com>
2023-11-06 20:33:09 +0100

    Applying Blog Form Styles

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -55,7 +55,8 @@ export default class BlogForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="blog-form-wrapper ">
+        <div className="two-column">
         <input
           type="text"
           onChange={this.handleChange}
@@ -71,8 +72,9 @@ export default class BlogForm extends Component {
           placeholder="Blog status"
           value={this.state.blog_status}
         />
+        </div>
 
-        <button>Save</button>
+        <button className="btn">Save</button>
       </form>
     );
   }
