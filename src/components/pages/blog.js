ce93a3bfd06c08b8f6c8f7951d24e17d7daa6e78
Marcel Rubin <marcelgabber89@gmail.com>
2023-11-06 16:50:04 +0100

    Building the Blog Form Event Handlers

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -59,7 +59,7 @@ class Blog extends Component {
 
     axios
       .get(
-        `https://jordan.devcamp.space/portfolio/portfolio_blogs?page=${this
+        `https://marcelrubin.devcamp.space/portfolio/portfolio_blogs?page=${this
           .state.currentPage}`,
         {
           withCredentials: true
